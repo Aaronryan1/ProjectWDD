b88e4150b9897f1b11c8de9c1436b4550962f55b
Aaron Ryan <aaronr6666@gmail.com>
2023-12-08 14:05:32 +0000

    ProjectWDD

---
+++ script.js
@@ -0,0 +1,28 @@
+const randomNumber = Math.floor(Math.random() * 20) + 1;
+let guesses = 3;
+
+function checkGuess() {
+    const userGuess = parseInt(prompt('Guess a number between 1 and 20:'));
+    
+    if (userGuess === randomNumber) {
+        alert('Congratulations! You guessed the correct number.');
+    } else {
+        guesses--;
+        if (guesses > 0) {
+            let message = 'Incorrect guess. ';
+            if (Math.abs(userGuess - randomNumber) <= 2) {
+                message += 'You are close to the correct number.';
+            } else {
+                if (userGuess > randomNumber) {
+                    message += 'Try a smaller number.';
+                } else {
+                    message += 'Try a larger number.';
+                }
+            }
+            message += ' You have ' + guesses + ' guesses remaining.';
+            alert(message);
+        } else {
+            alert('Sorry, you are out of guesses. The correct number was ' + randomNumber + '.');
+        }
+    }
+}
